ce766fdfd324013fcef65e2f688605e9e5ce1613
Srikanth Anantharam <sria91@gmail.com>
2018-01-22 20:27:30 +0530

    Corrected copyright declaration & added heading numbers to src/README.md
    Corrected copyright declaration & added heading numbers to src/README.md

--- tests/ctype/test_iscntrl.c
+++ tests/ctype/test_iscntrl.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018 Synapse Research Innovation
+ * Copyright © 2018-NOW Synapse Research Innovation
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
