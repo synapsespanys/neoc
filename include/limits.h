e7fa071687934a4e524fef612890e989b72220b1
Srikanth Anantharam <sria91@gmail.com>
2018-01-31 00:26:19 +0530

    Fixed limits.h.
    Fixed limits.h

--- include/limits.h
+++ include/limits.h
@@ -26,40 +26,40 @@
 #pragma once
 
 //! Number of bits for smallest object that is not a bit-field (byte)
-#define CHAR_BIT    8
+#define CHAR_BIT 8
 //! Minimum value for an object of type <tt>signed char</tt>
-#define SCHAR_MIN  -127  // -(2^7 - 1)
+#define SCHAR_MIN (-127)  // -(2^7 - 1)
 //! Maximum value for an object of type <tt>signed char</tt>
-#define SCHAR_MAX  +127  // 2^7 - 1
+#define SCHAR_MAX (+127)  // 2^7 - 1
 //! Maximum value for an object of type <tt>unsigned char</tt>
-#define UCHAR_MAX   255  // 2^8 - 1
+#define UCHAR_MAX 255  // 2^8 - 1
 //! Minimum value for an object of type <tt>char</tt>
-#define CHAR_MIN SCHAR_MIN
+#define CHAR_MIN 0
 //! Maximum value for an object of type <tt>char</tt>
-#define CHAR_MAX SCHAR_MAX
+#define CHAR_MAX UCHAR_MAX
 //! Maximum number of bytes in a multibyte character, for any supported locale
-#define MB_LEN_MAX  1
+#define MB_LEN_MAX 1
 //! Minimum value for an object of type <tt>short int</tt>
-#define SHRT_MIN -32767  // -(2^15 - 1)
+#define SHRT_MIN (-32767)  // -(2^15 - 1)
 //! Maximum value for an object of type <tt>short int</tt>
-#define SHRT_MAX +32767  // 2^15 - 1
+#define SHRT_MAX (+32767)  // 2^15 - 1
 //! Maximum value for an object of type <tt>unsigned short int</tt>
 #define USHRT_MAX 65535U  // 2^16 - 1
 //! Minimum value for an object of type <tt>int</tt>
-#define INT_MIN SHRT_MIN
+#define INT_MIN (-32767)
 //! Maximum value for an object of type <tt>int</tt>
-#define INT_MAX SHRT_MAX
+#define INT_MAX (+32767)
 //! Maximum value for an object of type <tt>unsigned int</tt>
-#define UINT_MAX USHRT_MAX
+#define UINT_MAX 65535
 //! Minimum value for an object of type <tt>long int</tt>
-#define LONG_MIN -2147483647L  // -(2^31 - 1)
+#define LONG_MIN (-2147483647L)  // -(2^31 - 1)
 //! Maximum value for an object of type <tt>long int</tt>
-#define LONG_MAX +2147483647L  // 2^31 - 1
+#define LONG_MAX (+2147483647L)  // 2^31 - 1
 //! Maximum value for an object of type <tt>unsigned long int</tt>
 #define ULONG_MAX 4294967295UL  // 2^32 - 1
 //! Minimum value for an object of type <tt>long long int</tt>
-#define LLONG_MIN -9223372036854775807LL  // -(2^63 - 1)
+#define LLONG_MIN (-9223372036854775807LL)  // -(2^63 - 1)
 //! Maximum value for an object of type <tt>long long int</tt>
-#define LLONG_MAX +9223372036854775807LL  // 2^63 - 1
+#define LLONG_MAX (+9223372036854775807LL)  // 2^63 - 1
 //! Maximum value for an object of type <tt>unsigned long long int</tt>
 #define ULLONG_MAX 18446744073709551615ULL  // 2^64 - 1
